662be5aaef21cbf8f38cefd3abf8d953d802b419
Lucas <lucasfelicio378@gmail.com>
2022-06-09 23:57:09 -0300

    bordas verder prato

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
-const button = document.querySelector('.buton-footer');
 
 function mudarButton(){
-    alert("ta indo");
+    const button = document.querySelector('.buton-footer');
+    button.classList.remove("buton-footer");
+    button.innerHTML = "Fechar pedido";
     
+}
+
+function clicarPrato1(){
+    const mudarBox = document.querySelector('.prato1');
+    mudarBox.classList.add("box-selecionado");
+}
+
+function clicarPrato2(){
+    const mudarBox = document.querySelector('.prato2');
+    mudarBox.classList.add("box-selecionado");
+}
+
+function clicarPrato3(){
+    const mudarBox = document.querySelector('.prato3');
+    mudarBox.classList.add("box-selecionado");
+}
+
+function clicarPrato4(){
+    const mudarBox = document.querySelector('.prato4');
+    mudarBox.classList.add("box-selecionado");
+}
+
+function clicarPrato5(){
+    const mudarBox = document.querySelector('.prato5');
+    mudarBox.classList.add("box-selecionado");
 }
